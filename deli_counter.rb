66dc8b2abff52cc2df4008c7ae036afeaac9d3cc
PARDEEP KAUR <pkaur84@aol.com>
2020-05-11 01:47:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,30 @@
 # Write your code here.
-def now_serving(array)
-  if array.empty? == true 
-    puts "There is nobody waiting to be served!"
-  elsif array.empty? == false 
-    puts "Currently serving #{array.shift}."
-  end 
-end 
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
-now_serving(katz_deli)
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+
+end
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
+
+
+end
